5164f23dd7f7ab0cf300baa940a4a997a2d1b20d
LFWinther <lfilipewborges@gmail.com>
2023-03-20 23:08:37 -0300

    atualização projetos|habilidades + css

---
+++ JS/habilidades.js
@@ -0,0 +1,299 @@
+var habilidade1 = document.querySelector('#card__habilidade-1');
+
+habilidade1.addEventListener("mouseover", function() {
+    var nome = document.querySelector("#m1");
+    var descricao = document.querySelector("#e1");
+    
+    nome.classList.remove("descricao");
+    descricao.classList.remove("descricao");
+});
+
+habilidade1.addEventListener("mouseout", function() {
+    var nome = document.querySelector("#m1");
+    var descricao = document.querySelector("#e1");
+    
+    nome.classList.add("descricao");
+    descricao.classList.add("descricao");
+});
+// -----------------------------------------
+
+
+var habilidade2 = document.querySelector('#card__habilidade-2');
+
+habilidade2.addEventListener("mouseover", function() {
+    var nome = document.querySelector("#m2");
+    var descricao = document.querySelector("#e2");
+    
+    nome.classList.remove("descricao");
+    descricao.classList.remove("descricao");
+});
+
+habilidade2.addEventListener("mouseout", function() {
+    var nome = document.querySelector("#m2");
+    var descricao = document.querySelector("#e2");
+    
+    nome.classList.add("descricao");
+    descricao.classList.add("descricao");
+});
+// -----------------------------------------
+
+
+var habilidade3 = document.querySelector('#card__habilidade-3');
+
+habilidade3.addEventListener("mouseover", function() {
+    var nome = document.querySelector("#m3");
+    var descricao = document.querySelector("#e3");
+    
+    nome.classList.remove("descricao");
+    descricao.classList.remove("descricao");
+});
+
+habilidade3.addEventListener("mouseout", function() {
+    var nome = document.querySelector("#m3");
+    var descricao = document.querySelector("#e3");
+    
+    nome.classList.add("descricao");
+    descricao.classList.add("descricao");
+});
+// -----------------------------------------
+
+
+var habilidade4 = document.querySelector('#card__habilidade-4');
+
+habilidade4.addEventListener("mouseover", function() {
+    var nome = document.querySelector("#m4");
+    var descricao = document.querySelector("#e4");
+    
+    nome.classList.remove("descricao");
+    descricao.classList.remove("descricao");
+});
+
+habilidade4.addEventListener("mouseout", function() {
+    var nome = document.querySelector("#m4");
+    var descricao = document.querySelector("#e4");
+    
+    nome.classList.add("descricao");
+    descricao.classList.add("descricao");
+});
+// -----------------------------------------
+
+
+var habilidade5 = document.querySelector('#card__habilidade-5');
+
+habilidade5.addEventListener("mouseover", function() {
+    var nome = document.querySelector("#m5");
+    var descricao = document.querySelector("#e5");
+    
+    nome.classList.remove("descricao");
+    descricao.classList.remove("descricao");
+});
+
+habilidade5.addEventListener("mouseout", function() {
+    var nome = document.querySelector("#m5");
+    var descricao = document.querySelector("#e5");
+    
+    nome.classList.add("descricao");
+    descricao.classList.add("descricao");
+});
+// -----------------------------------------
+
+
+var habilidade6 = document.querySelector('#card__habilidade-6');
+
+habilidade6.addEventListener("mouseover", function() {
+    var nome = document.querySelector("#m6");
+    var descricao = document.querySelector("#e6");
+    
+    nome.classList.remove("descricao");
+    descricao.classList.remove("descricao");
+});
+
+habilidade6.addEventListener("mouseout", function() {
+    var nome = document.querySelector("#m6");
+    var descricao = document.querySelector("#e6");
+    
+    nome.classList.add("descricao");
+    descricao.classList.add("descricao");
+});
+// -----------------------------------------
+
+
+var habilidade7= document.querySelector('#card__habilidade-7');
+
+habilidade7.addEventListener("mouseover", function() {
+    var nome = document.querySelector("#m7");
+    var descricao = document.querySelector("#e7");
+    
+    nome.classList.remove("descricao");
+    descricao.classList.remove("descricao");
+});
+
+habilidade7.addEventListener("mouseout", function() {
+    var nome = document.querySelector("#m7");
+    var descricao = document.querySelector("#e7");
+    
+    nome.classList.add("descricao");
+    descricao.classList.add("descricao");
+});
+// -----------------------------------------
+
+
+var habilidade8 = document.querySelector('#card__habilidade-8');
+
+habilidade8.addEventListener("mouseover", function() {
+    var nome = document.querySelector("#m8");
+    var descricao = document.querySelector("#e8");
+    
+    nome.classList.remove("descricao");
+    descricao.classList.remove("descricao");
+});
+
+habilidade8.addEventListener("mouseout", function() {
+    var nome = document.querySelector("#m8");
+    var descricao = document.querySelector("#e8");
+    
+    nome.classList.add("descricao");
+    descricao.classList.add("descricao");
+});
+// -----------------------------------------
+
+
+var habilidade9 = document.querySelector('#card__habilidade-9');
+
+habilidade9.addEventListener("mouseover", function() {
+    var nome = document.querySelector("#m9");
+    var descricao = document.querySelector("#e9");
+    
+    nome.classList.remove("descricao");
+    descricao.classList.remove("descricao");
+});
+
+habilidade9.addEventListener("mouseout", function() {
+    var nome = document.querySelector("#m9");
+    var descricao = document.querySelector("#e9");
+    
+    nome.classList.add("descricao");
+    descricao.classList.add("descricao");
+});
+// -----------------------------------------
+
+
+var habilidade10 = document.querySelector('#card__habilidade-10');
+
+habilidade10.addEventListener("mouseover", function() {
+    var nome = document.querySelector("#m10");
+    var descricao = document.querySelector("#e10");
+    
+    nome.classList.remove("descricao");
+    descricao.classList.remove("descricao");
+});
+
+habilidade10.addEventListener("mouseout", function() {
+    var nome = document.querySelector("#m10");
+    var descricao = document.querySelector("#e10");
+    
+    nome.classList.add("descricao");
+    descricao.classList.add("descricao");
+});
+// -----------------------------------------
+
+
+var habilidade11 = document.querySelector('#card__habilidade-11');
+
+habilidade11.addEventListener("mouseover", function() {
+    var nome = document.querySelector("#m11");
+    var descricao = document.querySelector("#e11");
+    
+    nome.classList.remove("descricao");
+    descricao.classList.remove("descricao");
+});
+
+habilidade11.addEventListener("mouseout", function() {
+    var nome = document.querySelector("#m11");
+    var descricao = document.querySelector("#e11");
+    
+    nome.classList.add("descricao");
+    descricao.classList.add("descricao");
+});
+// -----------------------------------------
+
+
+var habilidade12 = document.querySelector('#card__habilidade-12');
+
+habilidade12.addEventListener("mouseover", function() {
+    var nome = document.querySelector("#m12");
+    var descricao = document.querySelector("#e12");
+    
+    nome.classList.remove("descricao");
+    descricao.classList.remove("descricao");
+});
+
+habilidade12.addEventListener("mouseout", function() {
+    var nome = document.querySelector("#m12");
+    var descricao = document.querySelector("#e12");
+    
+    nome.classList.add("descricao");
+    descricao.classList.add("descricao");
+});
+// -----------------------------------------
+
+
+var habilidade13 = document.querySelector('#card__habilidade-13');
+
+habilidade13.addEventListener("mouseover", function() {
+    var nome = document.querySelector("#m13");
+    var descricao = document.querySelector("#e13");
+    
+    nome.classList.remove("descricao");
+    descricao.classList.remove("descricao");
+});
+
+habilidade13.addEventListener("mouseout", function() {
+    var nome = document.querySelector("#m13");
+    var descricao = document.querySelector("#e13");
+    
+    nome.classList.add("descricao");
+    descricao.classList.add("descricao");
+});
+// -----------------------------------------
+
+
+var habilidade14 = document.querySelector('#card__habilidade-14');
+
+habilidade14.addEventListener("mouseover", function() {
+    var nome = document.querySelector("#m14");
+    var descricao = document.querySelector("#e14");
+    
+    nome.classList.remove("descricao");
+    descricao.classList.remove("descricao");
+});
+
+habilidade14.addEventListener("mouseout", function() {
+    var nome = document.querySelector("#m14");
+    var descricao = document.querySelector("#e14");
+    
+    nome.classList.add("descricao");
+    descricao.classList.add("descricao");
+});
+// -----------------------------------------
+
+
+var habilidade15 = document.querySelector('#card__habilidade-15');
+
+habilidade15.addEventListener("mouseover", function() {
+    var nome = document.querySelector("#m15");
+    var descricao = document.querySelector("#e15");
+    
+    nome.classList.remove("descricao");
+    descricao.classList.remove("descricao");
+});
+
+habilidade15.addEventListener("mouseout", function() {
+    var nome = document.querySelector("#m15");
+    var descricao = document.querySelector("#e15");
+    
+    nome.classList.add("descricao");
+    descricao.classList.add("descricao");
+});
+// -----------------------------------------
+
---
+++ JS/projetos.js
@@ -0,0 +1,125 @@
+var projeto1 = document.querySelector('#card__projeto-1');
+
+projeto1.addEventListener("mouseover", function() {
+    var nome = document.querySelector("#n1");
+    var descricao = document.querySelector("#d1");
+    
+    nome.classList.remove("descricao");
+    descricao.classList.remove("descricao");
+});
+
+projeto1.addEventListener("mouseout", function() {
+    var nome = document.querySelector("#n1");
+    var descricao = document.querySelector("#d1");
+    
+    nome.classList.add("descricao");
+    descricao.classList.add("descricao");
+});
+// -----------------------------------------
+
+
+
+var projeto2 = document.querySelector('#card__projeto-2');
+
+projeto2.addEventListener("mouseover", function() {
+    var nome = document.querySelector("#n2");
+    var descricao = document.querySelector("#d2");
+    
+    nome.classList.remove("descricao");
+    descricao.classList.remove("descricao");
+});
+
+projeto2.addEventListener("mouseout", function() {
+    var nome = document.querySelector("#n2");
+    var descricao = document.querySelector("#d2");
+    
+    nome.classList.add("descricao");
+    descricao.classList.add("descricao");
+});
+// -----------------------------------------
+
+
+
+var projeto3 = document.querySelector('#card__projeto-3');
+
+projeto3.addEventListener("mouseover", function() {
+    var nome = document.querySelector("#n3");
+    var descricao = document.querySelector("#d3");
+    
+    nome.classList.remove("descricao");
+    descricao.classList.remove("descricao");
+});
+
+projeto3.addEventListener("mouseout", function() {
+    var nome = document.querySelector("#n3");
+    var descricao = document.querySelector("#d3");
+    
+    nome.classList.add("descricao");
+    descricao.classList.add("descricao");
+});
+// -----------------------------------------
+
+
+
+var projeto4 = document.querySelector('#card__projeto-4');
+
+projeto4.addEventListener("mouseover", function() {
+    var nome = document.querySelector("#n4");
+    var descricao = document.querySelector("#d4");
+    
+    nome.classList.remove("descricao");
+    descricao.classList.remove("descricao");
+});
+
+projeto4.addEventListener("mouseout", function() {
+    var nome = document.querySelector("#n4");
+    var descricao = document.querySelector("#d4");
+    
+    nome.classList.add("descricao");
+    descricao.classList.add("descricao");
+});
+// -----------------------------------------
+
+
+
+var projeto5 = document.querySelector('#card__projeto-5');
+
+projeto5.addEventListener("mouseover", function() {
+    var nome = document.querySelector("#n5");
+    var descricao = document.querySelector("#d5");
+    
+    nome.classList.remove("descricao");
+    descricao.classList.remove("descricao");
+});
+
+projeto5.addEventListener("mouseout", function() {
+    var nome = document.querySelector("#n5");
+    var descricao = document.querySelector("#d5");
+    
+    nome.classList.add("descricao");
+    descricao.classList.add("descricao");
+});
+// -----------------------------------------
+
+
+
+var projeto6 = document.querySelector('#card__projeto-6');
+
+projeto6.addEventListener("mouseover", function() {
+    var nome = document.querySelector("#n6");
+    var descricao = document.querySelector("#d6");
+    
+    nome.classList.remove("descricao");
+    descricao.classList.remove("descricao");
+});
+
+projeto6.addEventListener("mouseout", function() {
+    var nome = document.querySelector("#n6");
+    var descricao = document.querySelector("#d6");
+    
+    nome.classList.add("descricao");
+    descricao.classList.add("descricao");
+});
+// -----------------------------------------
+
+
